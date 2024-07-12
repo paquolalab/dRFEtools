27c49c8bc85dfaa9dee7b386078bf8930f4670c5
KrotosBenjamin <kj.benjamin90@gmail.com>
2024-07-12 16:20:44 -0400

    updating annotations

--- dRFEtools/random_forest.py
+++ dRFEtools/random_forest.py
@@ -1,34 +1,36 @@
 """
-This package has several function to run feature elimination for
-random forest, it will use the out-of-bag (OOB), assuming
-OOB set to True. Three measurements are calculated for feature selection.
+This package provides several functions to run feature elimination for
+random forest models, assuming OOB (out-of-bag) is set to True. Three
+measurements are calculated for feature selection:
 
 1. Normalized mutual information
 2. Accuracy
 3. Area under the curve (AUC) ROC curve
 
-Original author Apua Paquola.
-Edits: Kynon Jade Benjamin
-Feature ranking modified from Tarun Katipalli ranking function.
+Original author: Apuã Paquola.
+Edits and maintenance: Kynon Jade Benjamin.
+Feature ranking modified from Tarun Katipalli's ranking function.
 
-Additional modification by KJB to replace class definitions of functions.
+Additional modifications by Kynon Jade Benjamin to replace class definitions of
+functions.
 """
 
 __author__ = 'Kynon Jade Benjamin'
 
 import numpy as np
 from itertools import chain
-from sklearn.metrics import r2_score
-from sklearn.metrics import roc_auc_score
-from sklearn.metrics import accuracy_score
-from .rank_function import features_rank_fnc
-from sklearn.metrics import mean_squared_error
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import (
+    r2_score,
+    roc_auc_score,
+    accuracy_score,
+    mean_squared_error,
+    explained_variance_score,
+    normalized_mutual_info_score
+)
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import explained_variance_score
 from sklearn.inspection import permutation_importance
-from sklearn.metrics import normalized_mutual_info_score
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
+from .rank_function import features_rank_fnc
 
 
 def oob_predictions(estimator):
@@ -37,10 +39,10 @@ def oob_predictions(estimator):
     classifier classes.
 
     Args:
-    estimator: Random forest classifier or regressor object
+        estimator: Random forest classifier or regressor object
 
-    Yields:
-    vector: OOB predicted labels
+    Returns:
+        vector: OOB predicted labels
     """
     if isinstance(estimator, RandomForestClassifier):
         return estimator.classes_[(estimator.oob_decision_function_[:, 1]
@@ -55,11 +57,11 @@ def oob_score_roc(estimator, Y):
     for the OOB predictions.
 
     Args:
-    estimator: Random forest classifier object
-    Y: a vector of sample labels from training data set
+        estimator: Random forest classifier object
+        Y: a vector of sample labels from training data set
 
-    Yields:
-    float: AUC ROC score
+    Returns:
+        float: AUC ROC score
     """
     if len(np.unique(Y)) > 2:
         labels_pred = estimator.oob_decision_function_
@@ -76,11 +78,11 @@ def oob_score_nmi(estimator, Y):
     from the OOB predictions.
 
     Args:
-    estimator: Random forest classifier object
-    Y: a vector of sample labels from training data set
+        estimator: Random forest classifier object
+        Y: a vector of sample labels from training data set
 
-    Yields:
-    float: normalized mutual information score
+    Returns:
+        float: normalized mutual information score
     """
     labels_pred = oob_predictions(estimator)
     return normalized_mutual_info_score(Y, labels_pred,
@@ -92,11 +94,11 @@ def oob_score_accuracy(estimator, Y):
     Calculates the accuracy score from the OOB predictions.
 
     Args:
-    estimator: Random forest classifier object
-    Y: a vector of sample labels from training data set
+        estimator: Random forest classifier object
+        Y: a vector of sample labels from training data set
 
-    Yields:
-    float: accuracy score
+    Returns:
+        float: accuracy score
     """
     labels_pred = oob_predictions(estimator)
     return accuracy_score(Y, labels_pred)
@@ -107,11 +109,11 @@ def oob_score_r2(estimator, Y):
     Calculates the R2 score from the OOB predictions.
 
     Args:
-    estimator: Random forest regressor object
-    Y: a vector of sample labels from training data set
+        estimator: Random forest regressor object
+        Y: a vector of sample labels from training data set
 
-    Yields:
-    float: R2 score
+    Returns:
+        float: R2 score
     """
     labels_pred = oob_predictions(estimator)
     return r2_score(Y, labels_pred)
@@ -122,11 +124,11 @@ def oob_score_mse(estimator, Y):
     Calculates the mean square error from the OOB predictions.
 
     Args:
-    estimator: Random forest regressor object
-    Y: a vector of sample labels from training data set
+        estimator: Random forest regressor object
+        Y: a vector of sample labels from training data set
 
-    Yields:
-    float: mean square error
+    Returns:
+        float: mean square error
     """
     labels_pred = oob_predictions(estimator)
     return mean_squared_error(Y, labels_pred)
@@ -137,11 +139,11 @@ def oob_score_evar(estimator, Y):
     Calculates the explained variance score from the OOB predictions.
 
     Args:
-    estimator: Random forest regressor object
-    Y: a vector of sample labels from training data set
+        estimator: Random forest regressor object
+        Y: a vector of sample labels from training data set
 
-    Yields:
-    float: explained variance score
+    Returns:
+        float: explained variance score
     """
     labels_pred = oob_predictions(estimator)
     return explained_variance_score(Y, labels_pred,
@@ -151,29 +153,24 @@ def oob_score_evar(estimator, Y):
 def rf_fe_step(estimator, X, Y, n_features_to_keep, features, fold, out_dir,
                RANK):
     """
-    Apply random forest to training data, rank features, conduct feature
-    elimination.
+    Eliminates features step-by-step.
 
     Args:
-    estimator: Random forest classifier object
-    X: a data frame of training data
-    Y: a vector of sample labels from training data set
-    n_features_to_keep: number of features to keep
-    features: a vector of feature names
-    fold: current fold
-    out_dir: output directory. default '.'
+        estimator: Random forest classifier object
+        X: a data frame of training data
+        Y: a vector of sample labels from training data set
+        n_features_to_keep: number of features to keep
+        features: a vector of feature names
+        fold: current fold
+        out_dir: output directory. default '.'
+        RANK: Boolean (True/False) to return ranks
 
-    Yields:
-    dict: a dictionary with number of features, normalized mutual
-          information score, accuracy score, auc roc score and selected features
+    Returns:
+        dict: a dictionary containing feature elimination results
     """
-    # kwargs = {'random_state': 13, 'test_size': 0.20}
-    # X1, X2, Y1, Y2 = train_test_split(X, Y, **kwargs)
     assert n_features_to_keep <= X.shape[1]
     estimator.fit(X, Y)
     test_indices = np.array(range(X.shape[1]))
-    #res = permutation_importance(estimator, X2, Y2, n_jobs=-1, random_state=13)
-    #rank = test_indices[res.importances_mean.argsort()]
     rank = test_indices[np.argsort(estimator.feature_importances_)]
     rank = rank[::-1] # reverse sort
     selected = rank[0:n_features_to_keep]
@@ -183,32 +180,33 @@ def rf_fe_step(estimator, X, Y, n_features_to_keep, features, fold, out_dir,
                 'nmi_score': oob_score_nmi(estimator, Y),
                 'accuracy_score': oob_score_accuracy(estimator, Y),
                 'roc_auc_score': oob_score_roc(estimator, Y),
-                'selected': selected}
+                'selected': selected
+                }
     else:
         return {'n_features': X.shape[1],
                 'r2_score': oob_score_r2(estimator, Y),
                 'mse_score': oob_score_mse(estimator, Y),
                 'explain_var': oob_score_evar(estimator, Y),
-                'selected': selected}
+                'selected': selected
+                }
 
 
 def rf_fe(estimator, X, Y, n_features_iter, features, fold, out_dir, RANK):
     """
-    Iterate over features to by eliminated by step.
+    Iterates over features to be eliminated step-by-step.
 
     Args:
-    estimator: Random forest classifier or regressor object
-    X: a data frame of training data
-    Y: a vector of sample labels from training data set
-    n_features_iter: iterator for number of features to keep loop
-    features: a vector of feature names
-    fold: current fold
-    out_dir: output directory. default '.'
+        estimator: Random forest classifier or regressor object
+        X: a data frame of training data
+        Y: a vector of sample labels from training data set
+        n_features_iter: iterator for number of features to keep loop
+        features: a vector of feature names
+        fold: current fold
+        out_dir: output directory. default '.'
+        RANK: Boolean (True/False) to return ranks
 
-    Yields:
-    list: a list with number of features, normalized mutual
-          information score, accuracy score, auc roc curve and array of the
-          indices for features to keep
+    Returns:
+        list: list with feature elimination results
     """
     indices = np.array(range(X.shape[1]))
     for nf in chain(n_features_iter, [1]):
